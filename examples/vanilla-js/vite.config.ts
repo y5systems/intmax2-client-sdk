687aa1a56cc712943e783aa012d16abea5d1eab9
Alexandre Carvalheira <axxcar@gmail.com>
2025-02-20 09:46:25 -0300

    Fix: import paths (cjs/esm)

--- examples/vanilla-js/vite.config.ts
+++ examples/vanilla-js/vite.config.ts
@@ -3,9 +3,5 @@ import * as path from 'node:path';
 
 /// THIS FILE IS USED FOR EXAMPLE PURPOSES ONLY
 export default defineConfig({
-  resolve: {
-    alias: {
-      'intmax2-client-sdk': path.resolve(__dirname, '../../browser-sdk/dist'),
-    },
-  },
+  resolve: {},
 });

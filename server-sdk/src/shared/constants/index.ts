f0a7221c653745c8b986cfc0dca8809365961b83
Vlad <vlad@intmax.io>
2025-04-04 15:01:09 +0300

    v1.1.9: - update urls

--- server-sdk/src/shared/constants/index.ts
+++ server-sdk/src/shared/constants/index.ts
@@ -28,12 +28,12 @@ export const MAINNET_ENV: SDKUrls = {
 };
 
 export const TESTNET_ENV: SDKUrls = {
-  balance_prover_url: 'https://stage.private.zkp.intmax.io',
-  indexer_url: 'https://beta.stage.indexer.intmax.io/v1/indexer',
-  validity_prover_url: 'https://stage.node.api.intmax.io/validity-prover',
-  store_vault_server_url: 'https://stage.node.api.intmax.io/store-vault-server',
-  withdrawal_aggregator_url: 'https://stage.node.api.intmax.io/withdrawal-server',
-  predicate_url: 'https://beta.stage.predicate.intmax.io/v1/predicate',
+  balance_prover_url: 'https://stage.api.private.zkp.intmax.io',
+  indexer_url: 'https://stage.api.indexer.intmax.io/v1/indexer',
+  validity_prover_url: 'https://stage.api.node.intmax.io/validity-prover',
+  store_vault_server_url: 'https://stage.api.node.intmax.io/store-vault-server',
+  withdrawal_aggregator_url: 'https://stage.api.node.intmax.io/withdrawal-server',
+  predicate_url: 'https://stage.api.predicate.intmax.io/v1/predicate',
   chain_id_l1: 11155111,
   chain_id_l2: 534351,
   //
@@ -46,27 +46,27 @@ export const TESTNET_ENV: SDKUrls = {
   rpc_url_l1: 'https://sepolia.gateway.tenderly.co',
   rpc_url_l2: 'https://sepolia-rpc.scroll.io',
   key_vault_url: 'https://slxcnfhgxpfokwtathje.supabase.co/functions/v1/keyvault',
-  tokens_url: 'https://beta.stage.token.intmax.io',
+  tokens_url: 'https://stage.api.token.intmax.io',
 };
 
 export const DEVNET_ENV: SDKUrls = {
-  balance_prover_url: 'https://dev.private.zkp.intmax.xyz',
-  indexer_url: 'https://dev.indexer.intmax.xyz/v1/indexer',
-  validity_prover_url: 'https://dev.node.api.intmax.xyz/validity-prover',
-  store_vault_server_url: 'https://dev.node.api.intmax.xyz/store-vault-server',
-  withdrawal_aggregator_url: 'https://dev.node.api.intmax.xyz/withdrawal-server',
-  predicate_url: 'https://dev.predicate.intmax.xyz/v1/predicate',
+  balance_prover_url: 'https://dev.api.private.zkp.intmax.xyz',
+  indexer_url: 'https://dev.api.indexer.intmax.xyz/v1/indexer',
+  validity_prover_url: 'https://dev.api.node.intmax.xyz/validity-prover',
+  store_vault_server_url: 'https://dev.api.node.intmax.xyz/store-vault-server',
+  withdrawal_aggregator_url: 'https://dev.api.node.intmax.xyz/withdrawal-server',
+  predicate_url: 'https://dev.api.predicate.intmax.xyz/v1/predicate',
   chain_id_l1: 11155111,
   chain_id_l2: 534351,
   //
-  liquidity_contract: '0x9e60b235CF6d6cb63670b3fFC4f98fB56ba91409',
-  rollup_contract: '0xD315383e78E8dDee60e3BdaD8b278e5f10386557',
-  withdrawal_contract_address: '0xC2AD2B9720330D005B137f3A841F343C60A7a2F6',
+  liquidity_contract: '0xCD67eC9f3842F9f0E124C4dc7701043550580D67',
+  rollup_contract: '0xf6f77Dd98D746c052c1E3ff32c2f896c0B72045e',
+  withdrawal_contract_address: '0xcED4b1f2A52bAA23a0Cd599E4Ce7B6522838B0E3',
   predicate_contract_address: '0x4D9B3CF9Cb04B27C5D221c82B428D9dE990D3e3a',
   //
   rollup_contract_deployed_block_number: 8803282,
   rpc_url_l1: 'https://sepolia.gateway.tenderly.co',
   rpc_url_l2: 'https://sepolia-rpc.scroll.io',
   key_vault_url: 'https://oimhddprvflxjsumnmmg.supabase.co/functions/v1/keyvault',
-  tokens_url: 'https://dev.token.intmax.xyz/v1',
+  tokens_url: 'https://dev.api.token.intmax.xyz/v1',
 };
